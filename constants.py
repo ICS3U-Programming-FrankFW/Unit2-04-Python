247e37c8cde692a3ed0cdab6a27cdc2f406e2232
Frankie Fox Wagoosh <frank.fox-wagoosh@stu.ocsb.ca>
2022-09-29 12:55:18 +0000

    added pizza_cost.py

---
+++ constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Created by: Frankie fox
+# Created on: sept 29, 2022
+# This program contains constants.
+HST = 0.13
+LABOUR_COST = 2.00
+RENTAL_COST = 2.25
+INGRED_COST = 1.5
